a4d80f15451e8c0adb0827d0156cbc426d323236
Dayron-GLez <dayronglez01@gmail.com>
2023-06-21 00:15:50 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>100</h1>
+      <button>Increment</button>
+      <br />
+      <button>Decrement</button>
+    </div>
+  )
+}
---
+++ src/redux/store.ts
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit'
+import counterReducer from '@/redux/features/counterSlice'
+
+export const store = configureStore({
+    reducer: 
+    {
+        counterReducer
+    }
+})
